0dd5188b1744163906e26de57d9300e032a55cba
Guojize12 <jiecaomiao1@163.com>
2025-09-22 19:20:13 +0800

    Delete ota_pubkey.h

--- ota_pubkey.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-// 将下面内容替换成你的 ECDSA P-256 公钥（PEM）
-static const char OTA_PUBKEY_PEM[] PROGMEM =
-"-----BEGIN PUBLIC KEY-----\n"
-"REPLACE_WITH_YOUR_P256_PEM\n"
-"-----END PUBLIC KEY-----\n";
